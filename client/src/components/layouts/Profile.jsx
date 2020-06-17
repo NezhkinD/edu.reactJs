c83cf631062ac5b89642278ddccccb2ae7428093
NezhkinD <dnezhkin@gmail.com>
2020-06-17 20:48:06 +0300

    LES-14: shared module.css, add new styles

--- client/src/components/layouts/Profile.jsx
+++ client/src/components/layouts/Profile.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import classes from './Profile.module.css';
+
 
 const Profile = () => {
     return (
-        <div className={'content'}>
+        <div className={classes.content}>
             <img src={'https://html5css.ru/css/img_forest.jpg'}/>
             <div>ava + desc</div>
             <div>my posts</div>
             <div>new posts
-                <div>post 1</div>
-                <div>post 2</div>
+                <div className={classes.item}>post 1</div>
+                <div className={classes.item}>post 2</div>
             </div>
         </div>
     );
